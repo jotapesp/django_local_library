874e792b4cbea17e6a2a783ebf48048c53bfdb35
Joao <joaopedrodesanchespinheiro2@gmail.com>
2023-04-20 09:47:23 -0400

    put import to the top of the code

--- mhouse/settings.py
+++ mhouse/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,7 +77,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "mhouse.wsgi.application"
 
 
-# 
+#
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
@@ -127,7 +128,6 @@ LOGIN_REDIRECT_URL = "/"
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Update database configuration from $DATABASE_URL.
-import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
