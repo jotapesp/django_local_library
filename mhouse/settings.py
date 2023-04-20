dc728bd6a527ea96a258806384af4ff6fa85b2a9
Joao <joaopedrodesanchespinheiro2@gmail.com>
2023-04-20 12:55:24 -0400

    fixed stati files lines once more

--- mhouse/settings.py
+++ mhouse/settings.py
@@ -134,7 +134,9 @@ DATABASES['default'].update(db_from_env)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
